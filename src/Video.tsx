cf717318fe998b6c7ef5a4b940b4d334328f81e9
helopereira <helopereira689@gmail.com>
2024-02-10 16:02:50 -0300

    modified:   src/Video.tsx 	modified:   src/index.css

--- src/Video.tsx
+++ src/Video.tsx
@@ -4,6 +4,9 @@ import { MdPlayCircleFilled } from "react-icons/md";
 
 const Video = ({ video }) => {
     
+    function redirect (){
+        window.location.href=(video.link_botao)
+    }
 
     return (
         <div className={`
@@ -17,6 +20,12 @@ const Video = ({ video }) => {
             `}>
                 <div className="text-7xl text-zinc-800 font-black">{video?.titulo}</div>
                 <div className={`text-3xl text-zinc-700`}>{video?.subtitulo}</div>    
+
+                {video?.texto_botao && video?.link_botao && (
+                    <div className={`
+                        bg-pink-300/50 border-pink-300/50 rounded-md text-2xl p-3 botao
+                    `} onClick={() => redirect()}>{video.texto_botao}</div>
+                )}
             </div>
         </div>
     );
